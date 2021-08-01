26d40b16dd3949bc44fe5c10c84d16a8a0982757
Ori0112 <81377175+Ori0112@users.noreply.github.com>
2021-08-01 13:51:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ function updateFilters() {
     // 4b. Save the value that was changed as a variable.
     var changedVal = d3.select(this).property("value");
     // 4c. Save the id of the filter that was changed as a variable.
-    var changedId = d3.select(this).property("value");
+    var changedId = d3.select(this).property("id");
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (changedVal) {
-        filters[changedId] = changedVal;
+        filters_all[changedId] = changedVal;
     }
     else {
-        delete filters[changedId];
+        delete filters_all[changedId];
     };
   
     // 6. Call function to apply all filters and rebuild the table
